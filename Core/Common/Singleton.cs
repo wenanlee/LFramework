0862e191b3bdb50ea33e39f4f241f0467805a25a
Wenan <Wenan@DESKTOP-5JN67NN>
2019-04-29 15:07:06 +0800

    单例

---
+++ Core/Common/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFramework.Core.Common
+{
+    /// <summary>
+    /// 单例
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Singleton<T> where T:Singleton<T>,new()
+    {
+        private static T _instance = default(T);
+        public static T Instance
+        {
+            get
+            {
+                if (_instance==null)
+                {
+                    _instance = new T();
+                    _instance.InitSingleton();
+                }
+                return _instance;
+            }
+        }
+
+        private void InitSingleton()
+        {
+            
+        }
+    }
+}

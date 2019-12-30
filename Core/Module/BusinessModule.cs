be4917ecbb0d16fcf5988b8e32eb0edf1cdba095
Office <WenanLee@outlook.com>
2019-12-30 11:58:21 +0800

    下架

--- Core/Module/BusinessModule.cs
+++
@@ -1,7 +0,0 @@
-namespace LFramework.Core.Module
-{
-    public abstract class BusinessModule:Module
-    {
-        private string _name=null;
-    }
-}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LFramework
 A lite framework of unity
 还没有写完
+版权属于公司,所以做下架处理.
